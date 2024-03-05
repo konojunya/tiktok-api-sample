28c6d393f9d2e02ad2aadcf10693c2ffe9078f26
Junya Kono <junya03dance@gmail.com>
2024-03-06 00:50:35 +0900

    oauth

--- server.js
+++ server.js
@@ -1,35 +1,49 @@
-import { logDevReady } from "@remix-run/node";
+import { createRequestHandler } from "@remix-run/express";
 import { broadcastDevReady } from "@remix-run/node";
+import express from "express";
 import * as build from "./build/index.js";
-import { Hono } from "hono";
-import { remix } from "remix-hono/handler";
-import { serve } from "@hono/node-server";
+import { ExpressAuth } from "@auth/express";
+import TikTok from "@auth/express/providers/tiktok";
 
-if (process.env.NODE_ENV === "development") logDevReady(build);
+const app = express();
+const PORT = process.env.PORT || 5656;
+const CLIENT_KEY = process.env.CLIENT_KEY;
+const SERVER_ENDPOINT_REDIRECT =
+  "https://cc00-126-196-154-238.ngrok-free.app/callback";
+const CODE_VERIFIER = process.env.CODE_VERIFIER;
+const CODE_CHALLENGE = process.env.CODE_CHALLENGE;
 
-const app = new Hono();
-const port = process.env.PORT || 5656;
+app.use(express.static("public"));
 
-app.use(
-  "*",
-  remix({
-    build,
-    mode: process.env.NODE_ENV,
-    getLoadContext(c) {
-      return c.env;
-    },
-  })
-);
+// auth.js
+app.get("/auth/*", ExpressAuth({ providers: [TikTok] }));
 
-serve(
-  {
-    fetch: app.fetch,
-    port,
-  },
-  () => {
-    if (process.env.NODE_ENV === "development") {
-      broadcastDevReady(build);
-    }
-    console.log(`Server started on http://localhost:${port}`);
+// scratch oauth callback
+app.get("/oauth", (req, res) => {
+  const csrfState = Math.random().toString(36).substring(2);
+  res.cookie("csrfState", csrfState, { maxAge: 60000 });
+
+  const url = "https://www.tiktok.com/v2/auth/authorize";
+  const q = new URLSearchParams();
+  q.append("client_key", CLIENT_KEY);
+  // https://developers.tiktok.com/doc/tiktok-api-scopes/
+  q.append("scope", "user.info.basic,video.list");
+  q.append("response_type", "code");
+  q.append("redirect_uri", SERVER_ENDPOINT_REDIRECT);
+  q.append("state", csrfState);
+  // q.append("code_challenge", CODE_CHALLENGE);
+  // q.append("code_challenge_method", "S256");
+
+  const uri = `${url}?${q.toString()}`;
+
+  res.redirect(uri);
+});
+
+app.all("*", createRequestHandler({ build }));
+
+app.listen(PORT, () => {
+  if (process.env.NODE_ENV === "development") {
+    broadcastDevReady(build);
   }
-);
+  console.log(`Server started on http://localhost:${PORT}`);
+});
